191b76687457599d47ffc32040cfab1fe2150eeb
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-03-28 11:58:14 +0200

    Payment Respository

---
+++ Application/Services/PaymentRespository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Services
+{
+    public class PaymentRespository : IPaymentRepository
+    {
+        private readonly HotelApiContext _context;
+        private readonly IMapper _mapper;
+        //Service Injection or dependecy injection 
+        public PaymentRespository(HotelApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<PaymentResponseDto>> GetAllAsync()
+        {
+            var payments = await _context.Payments.ToListAsync();
+            return _mapper.Map<IEnumerable<PaymentResponseDto>>(payments);
+        }
+
+
+    }
+}
