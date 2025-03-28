ae72cf4abe385e2307d42575f8a57efa1a1aa5aa
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-03-28 10:26:12 +0200

    Add Migration

---
+++ Infrastructure/Migrations/20250328082426_addColoumnsOnPayment.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColoumnsOnPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyCode",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyName",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PaymentStatus",
+                table: "Payments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyCode",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "CurrencyName",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Payments");
+        }
+    }
+}
